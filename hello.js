7a9d134b6ce32ef6d8de7608bb8a109a838a9e1e
Hyphaene <hypsene0hotmail.com>
2015-12-17 11:51:19 +0100

    clean

--- hello.js
+++ hello.js
@@ -1,12 +1,1 @@
 alert("hello");
-ceci est un test de branch parallèle. (y)
-test test test test
-test test test test
-test test test test
-test test test test
-test test test test
-test test test test
-test test test test
-test test test test
-test test test test
-test test test test
